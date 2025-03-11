46f8602c5602d835564b5cc014c515cb277a87e0
Natalia S. Ippolitova <nippolitova@t1-consulting.ru>
2025-03-11 13:43:50 +0300

    Переделан класс в record

--- src/main/java/org/ippnat/model/dto/ProductResponse.java
+++ src/main/java/org/ippnat/model/dto/ProductResponse.java
@@ -1,21 +1,7 @@
 package org.ippnat.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString(exclude = "balance")
-public class ProductResponse {
-
-    private Long id;
-    private String accountId;
-    private String type;
-    private BigDecimal balance;
+public record ProductResponse (Long id, String accountId, String type, BigDecimal balance) {
 
 }
